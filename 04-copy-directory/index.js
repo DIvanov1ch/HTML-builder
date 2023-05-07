befb76d409f8a7aa757bf945df0fe3c9b3e1f691
Denis Trukhanovich <denis.trukhanovich@gmail.com>
2023-05-07 11:48:56 +0300

    feat: implement 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,31 @@
+const { error } = require('console');
+const { mkdir, copyFile, readdir, rm } = require('fs/promises');
+const { join } = require('path');
 
+const sourcePath = join(__dirname, 'files');
+const destinationPath = join(__dirname, 'files-copy');
+
+function copyDir(src, dest) {
+  mkdir(dest, { recursive: true }).catch(error);
+  
+  readdir(src).catch(error).then(files => {
+    files.forEach(file => {
+      const source = join(src, file);
+      const destination = join(dest, file);
+      copyFile(source, destination).catch(error);
+    });
+  });
+
+  readdir(dest).catch(error).then(files => {
+    files.forEach(file => {
+      readdir(src).catch(error).then(files => {
+        if (!files.includes(file)) {
+          const unexistFile = join(dest, file);
+          rm(unexistFile, { recursive: true }).catch(error);
+        }
+      });
+    });
+  });
+}
+
+copyDir(sourcePath, destinationPath);
